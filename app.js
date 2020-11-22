c0670704eb8e9fa346de1ba2e93a41a4eb288f9c
Sam <samuelmatthews96@gmail.com>
2020-11-22 19:08:26 +0000

    Set DOM elements from JS

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 window.addEventListener('load', ()=> {
     let long;
     let lat;
+    let tempDesc = document.querySelector('.temp-description');
+    let tempDegree = document.querySelector('.temp-degree');
+    let timezone = document.querySelector('.location-timezone');
 
     if(navigator.geolocation)
     {
@@ -11,7 +14,7 @@ window.addEventListener('load', ()=> {
 
             //const api = 'api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=630aa0a3ac3a1273f7ec813b0354d438';
 
-            const api = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=630aa0a3ac3a1273f7ec813b0354d438';
+            const api = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=630aa0a3ac3a1273f7ec813b0354d438&units=metric';
 
             fetch(api)
                 .then(response =>{
@@ -20,7 +23,10 @@ window.addEventListener('load', ()=> {
             .then(data =>{
                 console.log(data)
                 const {temp} = data.main;
-                console.log(temp);
+                const {description} = data.weather[0];
+                // set DOM elements using API
+                tempDegree.textContent = temp;
+                tempDesc.textContent = description;
             })
 
         });
